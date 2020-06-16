5a0bf4639ed1bede77fa900772a117c8dc3945e7
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-06-17 00:49:15 +0700

    Add customer

---
+++ application/controllers/Customers.php
@@ -0,0 +1,219 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Customers extends Admin_Controller 
+{
+	public function __construct()
+	{
+		parent::__construct();
+
+		$this->not_logged_in();
+
+		$this->data['page_title'] = 'Customers';
+
+		$this->load->model('model_customers');
+	}
+
+	/* 
+	* It only redirects to the manage product page and
+	*/
+	public function index()
+	{
+		if(!in_array('viewSupplier', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
+
+		$result = $this->model_customers->getSupplierData();
+
+		$this->data['results'] = $result;
+
+		$this->render_template('customers/index', $this->data);
+	}
+
+	/*
+	* Fetches the supplier data from the supplier table 
+	* this function is called from the datatable ajax function
+	*/
+	public function fetchSupplierData()
+	{
+		$result = array('data' => array());
+
+		$data = $this->model_customers->getSupplierData();
+		foreach ($data as $key => $value) {
+
+			// button
+			$buttons = '';
+
+			if(in_array('viewSupplier', $this->permission)) {
+				$buttons .= '<button type="button" class="btn btn-default" onclick="editSupplier('.$value['id'].')" data-toggle="modal" data-target="#editSupplierModal"><i class="fa fa-pencil"></i></button>';	
+			}
+			
+			if(in_array('deleteSupplier', $this->permission)) {
+				$buttons .= ' <button type="button" class="btn btn-default" onclick="removeSupplier('.$value['id'].')" data-toggle="modal" data-target="#removeSupplierModal"><i class="fa fa-trash"></i></button>
+				';
+			}				
+
+			$status = ($value['active'] == 1) ? '<span class="label label-success">Active</span>' : '<span class="label label-warning">Inactive</span>';
+
+			$result['data'][$key] = array(
+				$value['name'],
+				$value['address'],
+				$value['phone'],
+				$status,
+				$buttons
+			);
+		} // /foreach
+
+		echo json_encode($result);
+	}
+
+	/*
+	* It checks if it gets the supplier id and retreives
+	* the supplier information from the supplier model and 
+	* returns the data into json format. 
+	* This function is invoked from the view page.
+	*/
+	public function fetchSupplierDataById($id)
+	{
+		if($id) {
+			$data = $this->model_customers->getSupplierData($id);
+			echo json_encode($data);
+		}
+
+		return false;
+	}
+
+	/*
+	* Its checks the supplier form validation 
+	* and if the validation is successfully then it inserts the data into the database 
+	* and returns the json format operation messages
+	*/
+	public function create()
+	{
+
+		if(!in_array('createSupplier', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
+
+		$response = array();
+
+		$this->form_validation->set_rules('supplier_name', 'Supplier name', 'trim|required');
+		$this->form_validation->set_rules('active', 'Active', 'trim|required');
+
+		$this->form_validation->set_error_delimiters('<p class="text-danger">','</p>');
+
+        if ($this->form_validation->run() == TRUE) {
+        	$data = array(
+				'name' => $this->input->post('supplier_name'),
+				'address' => $this->input->post('address'),
+				'phone' => $this->input->post('phone'),
+        		'active' => $this->input->post('active'),	
+        	);
+
+        	$create = $this->model_customers->create($data);
+        	if($create == true) {
+        		$response['success'] = true;
+        		$response['messages'] = 'Succesfully created';
+        	}
+        	else {
+        		$response['success'] = false;
+        		$response['messages'] = 'Error in the database while creating the supplier information';			
+        	}
+        }
+        else {
+        	$response['success'] = false;
+        	foreach ($_POST as $key => $value) {
+        		$response['messages'][$key] = form_error($key);
+        	}
+        }
+
+        echo json_encode($response);
+
+	}
+
+	/*
+	* Its checks the supplier form validation 
+	* and if the validation is successfully then it updates the data into the database 
+	* and returns the json format operation messages
+	*/
+	public function update($id)
+	{
+		if(!in_array('updateSupplier', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
+
+		$response = array();
+
+		if($id) {
+			$this->form_validation->set_rules('edit_supplier_name', 'Supplier name', 'trim|required');
+			$this->form_validation->set_rules('edit_active', 'Active', 'trim|required');
+
+			$this->form_validation->set_error_delimiters('<p class="text-danger">','</p>');
+
+	        if ($this->form_validation->run() == TRUE) {
+	        	$data = array(
+					'name' => $this->input->post('edit_supplier_name'),
+					'address' => $this->input->post('edit_address'),
+					'phone' => $this->input->post('edit_phone'),
+	        		'active' => $this->input->post('edit_active'),	
+	        	);
+
+	        	$update = $this->model_customers->update($data, $id);
+	        	if($update == true) {
+	        		$response['success'] = true;
+	        		$response['messages'] = 'Succesfully updated';
+	        	}
+	        	else {
+	        		$response['success'] = false;
+	        		$response['messages'] = 'Error in the database while updated the supplier information';			
+	        	}
+	        }
+	        else {
+	        	$response['success'] = false;
+	        	foreach ($_POST as $key => $value) {
+	        		$response['messages'][$key] = form_error($key);
+	        	}
+	        }
+		}
+		else {
+			$response['success'] = false;
+    		$response['messages'] = 'Error please refresh the page again!!';
+		}
+
+		echo json_encode($response);
+	}
+
+	/*
+	* It removes the supplier information from the database 
+	* and returns the json format operation messages
+	*/
+	public function remove()
+	{
+		if(!in_array('deleteSupplier', $this->permission)) {
+			redirect('dashboard', 'refresh');
+		}
+		
+		$supplier_id = $this->input->post('supplier_id');
+		$response = array();
+		if($supplier_id) {
+			$delete = $this->model_customers->remove($supplier_id);
+
+			if($delete == true) {
+				$response['success'] = true;
+				$response['messages'] = "Successfully removed";	
+			}
+			else {
+				$response['success'] = false;
+				$response['messages'] = "Error in the database while removing the supplier information";
+			}
+		}
+		else {
+			$response['success'] = false;
+			$response['messages'] = "Refersh the page again!!";
+		}
+
+		echo json_encode($response);
+	}
+
+}
---
+++ application/models/Model_customers.php
@@ -0,0 +1,58 @@
+<?php 
+
+class Model_suppliers extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/*get the active customers information*/
+	public function getActiveCustomers()
+	{
+		$sql = "SELECT * FROM customers WHERE active = ?";
+		$query = $this->db->query($sql, array(1));
+		return $query->result_array();
+	}
+
+	/* get the customer data */
+	public function getCustomerData($id = null)
+	{
+		if($id) {
+			$sql = "SELECT * FROM customers WHERE id = ?";
+			$query = $this->db->query($sql, array($id));
+			return $query->row_array();
+		}
+
+		$sql = "SELECT * FROM customers";
+		$query = $this->db->query($sql);
+		return $query->result_array();
+	}
+
+	public function create($data)
+	{
+		if($data) {
+			$insert = $this->db->insert('customers', $data);
+			return ($insert == true) ? true : false;
+		}
+	}
+
+	public function update($data, $id)
+	{
+		if($data && $id) {
+			$this->db->where('id', $id);
+			$update = $this->db->update('customers', $data);
+			return ($update == true) ? true : false;
+		}
+	}
+
+	public function remove($id)
+	{
+		if($id) {
+			$this->db->where('id', $id);
+			$delete = $this->db->delete('customers');
+			return ($delete == true) ? true : false;
+		}
+	}
+
+}
