78aeb83cbc1a5073cdde3466fd082f414dfb4269
Kisusyenni Venessa <kisusyenni01@gmail.com>
2020-06-18 02:48:59 +0700

    New database

--- Database/stockms_2.sql
+++ Database/stockms_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2020 at 12:10 PM
+-- Generation Time: Jun 17, 2020 at 09:48 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -32,6 +32,16 @@ CREATE TABLE `attribute_value` (
   `value` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `attribute_value`
+--
+
+INSERT INTO `attribute_value` (`id`, `value`) VALUES
+(4, 'Blue'),
+(5, 'Red'),
+(6, 'Pink'),
+(7, 'Yellow');
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +53,15 @@ CREATE TABLE `brands` (
   `name` varchar(255) NOT NULL,
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `active`) VALUES
+(1, 'XX1', 1),
+(2, 'XXY', 1),
+(3, 'XXz', 1);
 
 -- --------------------------------------------------------
 
@@ -56,6 +75,35 @@ CREATE TABLE `categories` (
   `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `active`) VALUES
+(1, 'Tas', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` text NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `currency` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`id`, `name`, `address`, `phone`, `country`, `currency`) VALUES
+(1, 'Ndo Stores', 'test', '812345678', 'Indonesia', 'IDR');
+
 -- --------------------------------------------------------
 
 --
@@ -67,8 +115,21 @@ CREATE TABLE `customers` (
   `firstname` varchar(255) DEFAULT NULL,
   `lastname` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
-  `phone` int(11) DEFAULT NULL
+  `phone` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`id`, `firstname`, `lastname`, `address`, `phone`) VALUES
+(1, 'Andi', 'Andi', 'kembang sari', '812537485'),
+(6, 'Adina', 'Adina', 'jalan rujak', '81727394'),
+(7, 'Adina', 'Adin', 'jalan rujak', '871738492'),
+(8, 'Rina', 'Rina', 'jalan mangga', '118379394'),
+(9, 'Tono', 'Tono', 'jalan pisang', '183728292'),
+(10, 'Tina', 'Tini', 'Jln semanggi blok a', '81279304'),
+(11, 'Tina', 'Tina', 'Ruko Asri Damai blok k no 5', '0812345678');
 
 -- --------------------------------------------------------
 
@@ -82,6 +143,13 @@ CREATE TABLE `groups` (
   `permission` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `group_name`, `permission`) VALUES
+(1, 'administrator', 'a:36:{i:0;s:10:\"createUser\";i:1;s:10:\"updateUser\";i:2;s:8:\"viewUser\";i:3;s:10:\"deleteUser\";i:4;s:11:\"createGroup\";i:5;s:11:\"updateGroup\";i:6;s:9:\"viewGroup\";i:7;s:11:\"deleteGroup\";i:8;s:11:\"createBrand\";i:9;s:11:\"updateBrand\";i:10;s:9:\"viewBrand\";i:11;s:11:\"deleteBrand\";i:12;s:14:\"createCategory\";i:13;s:14:\"updateCategory\";i:14;s:12:\"viewCategory\";i:15;s:14:\"deleteCategory\";i:16;s:11:\"createStore\";i:17;s:11:\"updateStore\";i:18;s:9:\"viewStore\";i:19;s:11:\"deleteStore\";i:20;s:15:\"createAttribute\";i:21;s:15:\"updateAttribute\";i:22;s:13:\"viewAttribute\";i:23;s:15:\"deleteAttribute\";i:24;s:13:\"createProduct\";i:25;s:13:\"updateProduct\";i:26;s:11:\"viewProduct\";i:27;s:13:\"deleteProduct\";i:28;s:11:\"createOrder\";i:29;s:11:\"updateOrder\";i:30;s:9:\"viewOrder\";i:31;s:11:\"deleteOrder\";i:32;s:11:\"viewReports\";i:33;s:13:\"updateCompany\";i:34;s:11:\"viewProfile\";i:35;s:13:\"updateSetting\";}');
+
 -- --------------------------------------------------------
 
 --
@@ -92,13 +160,22 @@ CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `bill_no` varchar(255) NOT NULL,
   `customer_id` int(11) NOT NULL,
-  `ordered_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `paid_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `ordered_at` varchar(255) DEFAULT NULL,
+  `paid_at` varchar(255) NOT NULL,
+  `gross_amount` int(11) NOT NULL,
   `net_amount` int(11) NOT NULL,
+  `total_discount` int(11) NOT NULL,
   `paid_status` int(11) NOT NULL,
   `method` int(11) DEFAULT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `bill_no`, `customer_id`, `ordered_at`, `paid_at`, `gross_amount`, `net_amount`, `total_discount`, `paid_status`, `method`, `user_id`) VALUES
+(1, 'BILPR-CBFD', 11, '1592421328', '1592421445', 12000000, 11850000, 150000, 1, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -115,6 +192,13 @@ CREATE TABLE `orders_item` (
   `amount` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `orders_item`
+--
+
+INSERT INTO `orders_item` (`id`, `order_id`, `product_id`, `qty`, `rate`, `amount`) VALUES
+(2, 1, 49, 10, 1200000, 12000000);
+
 -- --------------------------------------------------------
 
 --
@@ -126,9 +210,10 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `sku` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
-  `disc` int(11) DEFAULT NULL,
+  `discount` int(11) DEFAULT 0,
   `qty` int(11) NOT NULL,
-  `attribute_value_id` int(11) DEFAULT NULL,
+  `image` text NOT NULL DEFAULT 'assets/images/product_image/default.png',
+  `attribute_value` text DEFAULT NULL,
   `description` text DEFAULT NULL,
   `brand_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
@@ -136,6 +221,15 @@ CREATE TABLE `products` (
   `supplier_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `sku`, `price`, `discount`, `qty`, `image`, `attribute_value`, `description`, `brand_id`, `category_id`, `availability`, `supplier_id`, `user_id`) VALUES
+(48, 'testtt', '121425436', 1221435, 0, -13, '<p>You did not select a file to upload.</p>', '[\"Blue\",\"Red\"]', '<p>test</p>', 1, 1, 1, 1, 2),
+(49, 'qwwerty', '12435465', 1200000, 1000, -21, '<p>You did not select a file to upload.</p>', '[\"Blue\",\"Red\",\"Pink\",\"Yellow\"]', '<p>test</p>', 1, 1, 1, 1, 2),
+(50, 'Channoul', '12356464', 2000000, 150000, 15, 'assets/images/product_image/5eea6fe948c41.jpg', '[\"Pink\",\"Yellow\"]', '<p>Test</p>', 2, 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -147,8 +241,17 @@ CREATE TABLE `suppliers` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `phone` varchar(255) NOT NULL
+  `phone` varchar(255) NOT NULL,
+  `active` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `suppliers`
+--
+
+INSERT INTO `suppliers` (`id`, `name`, `address`, `phone`, `active`) VALUES
+(1, 'ABC', 'test', '', 1),
+(2, 'BBc', 'test123', '123', 1);
 
 -- --------------------------------------------------------
 
@@ -168,6 +271,13 @@ CREATE TABLE `users` (
   `birthdate` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `firstname`, `lastname`, `phone`, `address`, `birthdate`) VALUES
+(1, 'admin', '$2y$10$yfi5nUQGXUZtMdl27dWAyOd/jMOmATBpiUvJDmUu9hJ5Ro6BE5wsK', 'admin@ndostore.com', 'admin', 'admin', '0812345678', 'Komplek Hijau Asri Blok A No 5', '2000-01-01');
+
 -- --------------------------------------------------------
 
 --
@@ -179,6 +289,13 @@ CREATE TABLE `user_group` (
   `user_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_group`
+--
+
+INSERT INTO `user_group` (`id`, `user_id`, `group_id`) VALUES
+(1, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -203,6 +320,12 @@ ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
@@ -220,7 +343,7 @@ ALTER TABLE `groups`
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
   ADD KEY `orders_ibfk_1` (`customer_id`),
-  ADD KEY `orders_ibfk_2` (`user_id`) USING BTREE;
+  ADD KEY `orders_ibfk_2` (`user_id`);
 
 --
 -- Indexes for table `orders_item`
@@ -240,8 +363,7 @@ ALTER TABLE `products`
   ADD KEY `products_ibfk_1` (`brand_id`),
   ADD KEY `products_ibfk_2` (`category_id`),
   ADD KEY `products_ibfk_3` (`supplier_id`),
-  ADD KEY `products_ibfk_4` (`user_id`),
-  ADD KEY `products_ibfk_5` (`attribute_value_id`);
+  ADD KEY `products_ibfk_4` (`user_id`);
 
 --
 -- Indexes for table `suppliers`
@@ -271,61 +393,73 @@ ALTER TABLE `user_group`
 -- AUTO_INCREMENT for table `attribute_value`
 --
 ALTER TABLE `attribute_value`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders_item`
 --
 ALTER TABLE `orders_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
 --
 ALTER TABLE `suppliers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user_group`
 --
 ALTER TABLE `user_group`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -352,8 +486,7 @@ ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `products_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `products_ibfk_5` FOREIGN KEY (`attribute_value_id`) REFERENCES `attribute_value` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `products_ibfk_4` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `user_group`
